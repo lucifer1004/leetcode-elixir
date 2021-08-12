522601f641023a8eb857fa3146b9fa1e19dd998b
Gabriel Wu <qqbbnease1004@126.com>
2021-08-12 23:01:58 +0800

    update: 0003. Longest Substring Without Repeating Characters

--- 0001_0100/0003_Longest_Substring.exs
+++ 0001_0100/0003_Longest_Substring.exs
@@ -7,19 +7,20 @@ defmodule Solution do
 
   @spec two_pointers(l :: [String], r :: [String], ans :: integer, mp :: map()) :: integer
   defp two_pointers(l, r, ans, mp) do
-    if r == [] do
-      ans
-    else
-      if MapSet.member?(mp, List.first(r)) do
+    cond do
+      r == [] ->
+        ans
+
+      MapSet.member?(mp, List.first(r)) ->
         two_pointers(l |> List.delete_at(0), r, ans, mp |> MapSet.delete(List.first(l)))
-      else
+
+      true ->
         two_pointers(
           l,
           r |> List.delete_at(0),
           max(ans, MapSet.size(mp) + 1),
           mp |> MapSet.put(List.first(r))
         )
-      end
     end
   end
 end
