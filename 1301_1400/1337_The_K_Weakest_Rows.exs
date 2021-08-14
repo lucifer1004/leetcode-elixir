36ef8934a5599b0ec544d1d85a17ad66f05f9f6c
Gabriel Wu <qqbbnease1004@126.com>
2021-08-14 10:52:43 +0800

    feat: update 1337

--- 1301_1400/1337_The_K_Weakest_Rows.exs
+++ 1301_1400/1337_The_K_Weakest_Rows.exs
@@ -2,10 +2,10 @@ defmodule Solution do
   @spec k_weakest_rows(mat :: [[integer]], k :: integer) :: [integer]
   def k_weakest_rows(mat, k) do
     mat
-    |> Enum.map(fn x -> x |> Enum.sum() end)
+    |> Enum.map(&Enum.sum/1)
     |> Enum.with_index()
     |> Enum.sort()
     |> Enum.take(k)
-    |> Enum.map(fn x -> x |> elem(1) end)
+    |> Enum.map(&elem(&1, 1))
   end
 end
