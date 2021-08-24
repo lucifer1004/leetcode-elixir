e496715769501ac96473661b8e77331a268993b5
Gabriel Wu <qqbbnease1004@126.com>
2021-08-24 08:03:55 +0800

    feat: update 0787

--- 0701_0800/0787_Cheapest_Flights_with_K_Stops.exs
+++ 0701_0800/0787_Cheapest_Flights_with_K_Stops.exs
@@ -1,4 +1,6 @@
 defmodule Solution do
+  @inf 1_000_000_000
+
   @spec find_cheapest_price(
           n :: integer,
           flights :: [[integer]],
@@ -14,23 +16,25 @@ defmodule Solution do
       end)
 
     mp =
-      for(u <- 0..(n - 1), m <- 0..(k + 1), into: %{}, do: {{u, m}, 1_000_000_000})
-      |> Map.put({src, 0}, 0)
-
-    mp =
       0..k
-      |> Enum.reduce(mp, fn m, mp ->
+      |> Enum.reduce(%{{src, 0} => 0}, fn m, mp ->
         0..(n - 1)
         |> Enum.reduce(mp, fn u, mp ->
-          adj
-          |> Map.get(u, [])
-          |> Enum.reduce(mp, fn {v, w}, mp ->
-            mp |> Map.update({v, m + 1}, 1_000_000_000, &min(&1, (mp |> Map.get({u, m})) + w))
-          end)
+          now = mp |> Map.get({u, m})
+
+          if now == nil,
+            do: mp,
+            else:
+              adj
+              |> Map.get(u, [])
+              |> Enum.reduce(mp, fn {v, w}, mp ->
+                mp |> Map.update({v, m + 1}, now + w, &min(&1, now + w))
+              end)
         end)
       end)
 
-    ans = 0..(k + 1) |> Enum.reduce(1_000_000_000, &min(mp |> Map.get({dst, &1}), &2))
-    if ans == 1_000_000_000, do: -1, else: ans
+    ans = 0..(k + 1) |> Enum.reduce(@inf, &min(mp |> Map.get({dst, &1}, @inf), &2))
+
+    if ans == @inf, do: -1, else: ans
   end
 end
